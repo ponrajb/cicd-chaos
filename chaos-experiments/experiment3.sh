436357c1e008500c3b332d3398c06f78aa95ed9c
Raj B. Perumal <ponrajb@gmail.com>
2020-10-07 19:11:36 -0400

    Experiment #3 retry - changed app name to my user id

--- chaos-experiments/experiment3.sh
+++ chaos-experiments/experiment3.sh
@@ -7,4 +7,4 @@ GREMLIN_API_KEY=$2
 CIRCLE_USERNAME=$3
 
 
-curl -i -X POST "https://api.gremlin.com/v1/attacks/new?teamId=${GREMLIN_TEAM_ID}" -H 'Content-Type: application/json;charset=utf-8' -H 'Authorization: Bearer Yy0zODZkNjNhMS1kMGI1LTU2ODYtYTdkZi1hMjA2OTRiYTBlNmI6Y29tbXVuaXR5K2NpcmNsZUBncmVtbGluLmNvbToxYzA5MWZhYS04NTdlLTQzMzEtODkxZi1hYTg1N2UzMzMxOGQ=' -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"app":["ana-medina-cicd-chaos-app"]}},"percent":100},"command":{"type":"latency","commandType":"Latency","args":["-l","300","-m","500","-h","^api.gremlin.com","-p","^53"],"providers":[]}}'
+curl -i -X POST "https://api.gremlin.com/v1/attacks/new?teamId=${GREMLIN_TEAM_ID}" -H 'Content-Type: application/json;charset=utf-8' -H 'Authorization: Bearer Yy0zODZkNjNhMS1kMGI1LTU2ODYtYTdkZi1hMjA2OTRiYTBlNmI6Y29tbXVuaXR5K2NpcmNsZUBncmVtbGluLmNvbToxYzA5MWZhYS04NTdlLTQzMzEtODkxZi1hYTg1N2UzMzMxOGQ=' -d '{"target":{"type":"Random","containers":{"multiSelectLabels":{"app":["raj-cicd-chaos-app"]}},"percent":100},"command":{"type":"latency","commandType":"Latency","args":["-l","300","-m","500","-h","^api.gremlin.com","-p","^53"],"providers":[]}}'
